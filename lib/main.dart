fc22152de1fd9f76549256a00c7a1164ee12d917
Felipe Bertanha <bertanhahp@gmail.com>
2019-09-26 23:30:14 -0300

    aula 2 ListView

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,56 @@
 import 'package:flutter/material.dart';
 
 void main() =>
-    runApp(
-      Column(
-        children: <Widget>[
-          Text(
-            'Bem vindo ao Bytebank',
-            textDirection: TextDirection.ltr,
-          ),
-          Text(
-            'Flutter text',
-            textDirection: TextDirection.ltr,
-          ),
-        ],
+    runApp(MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('ByteBank'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            print("rsrsrs");
+          },
+        ),
+        body: ListaTransferencias(),
       ),
+    ));
+
+class ListaTransferencias extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        ItemTransferencia(Transferencia(100.0, 'Conta: Felipe B.')),
+        ItemTransferencia(Transferencia(200.0, 'Conta: Evelyn P. Sanches'))
+      ],
     );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Card(
+        child: ListTile(
+          leading: Icon(Icons.monetization_on),
+          title: Text(_transferencia.valor.toString()),
+          subtitle: Text(_transferencia.numeroConta),
+          onTap: () {
+            print('Clicado: ${_transferencia.numeroConta}');
+          },
+        ));
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final String numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
+}
